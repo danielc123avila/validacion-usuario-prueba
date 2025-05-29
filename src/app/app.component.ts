d230365f928ba257234cbee130ae572e621825d1
daniel_colina <danielc123avila@icloud.com>
2025-05-29 10:35:06 -0500

    configuraciones de angular para poder terminar de correr el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { SearchFormComponent } from './components/search-form/search-form.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ SearchFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'validacion-usuario-prueba';
+  title = 'validacion-usuarios';
 }
